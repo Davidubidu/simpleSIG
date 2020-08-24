cc9928f84255bdd42b1a8b2c6e7dec6ce09c39bd
David <37439686+Davidubidu@users.noreply.github.com>
2020-08-24 17:11:31 +0200

    merging to dev
    update in mappoint, merging to dev

--- src/main/java/com/example/demo/model/MapPoint.java
+++ src/main/java/com/example/demo/model/MapPoint.java
@@ -14,52 +14,87 @@ public class MapPoint {
 	private int height;
 	private String type;
 	private String description;
+	private String ownername;
+	private boolean visible;
 		
-	public MapPoint(int xCoordinate, int yCoordinate, int height, String type, String description) {
+	
+	
+	public MapPoint(String id, int xCoordinate, int yCoordinate, int height, String type, String description,
+			String ownername, boolean visible) {
+		this.id = id;
 		this.xCoordinate = xCoordinate;
 		this.yCoordinate = yCoordinate;
 		this.height = height;
 		this.type = type;
 		this.description = description;
+		this.ownername = ownername;
+		this.visible = visible;
 	}
-	
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
+	public String getownername() {
+		return ownername;
+	}
+
+	public void setownername(String ownername) {
+		this.ownername = ownername;
+	}
+
 	public int getxCoordinate() {
 		return xCoordinate;
 	}
+	
 	public void setxCoordinate(int xCoordinate) {
 		this.xCoordinate = xCoordinate;
 	}
+	
 	public int getyCoordinate() {
 		return yCoordinate;
 	}
+	
 	public void setyCoordinate(int yCoordinate) {
 		this.yCoordinate = yCoordinate;
 	}
+	
 	public int getHeight() {
 		return height;
 	}
+	
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	
 	public String getType() {
 		return type;
 	}
+	
 	public void setType(String type) {
 		this.type = type;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -75,7 +110,13 @@ public class MapPoint {
 		builder.append(type);
 		builder.append(", description=");
 		builder.append(description);
+		builder.append(", ownername=");
+		builder.append(ownername);
+		builder.append(", visible:");
+		builder.append(isVisible());
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
 }
