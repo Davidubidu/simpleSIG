28872db12ff911cde4ca3cdf0b078aae92749fe8
TonMoshi <chingowamen@hotmail.com>
2020-09-30 19:36:28 +0200

    - Adding map componetn

---
+++ simpleSIGFront/src/app/components/modules/modules.module.ts
@@ -0,0 +1,13 @@
+import { Components } from './components';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [
+    Components
+  ],
+  imports: [],
+  exports: [ Components ],
+  providers: []
+})
+export class ModulesModule { }
